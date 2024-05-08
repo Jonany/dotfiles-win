b091d1c1f187063900fd0a48c3233a0c6678b7d6
Jonathan Rigsby <Jonathan Rigsby>
2024-05-08 15:16:11 -0400

    updated light mode colors for wezterm tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.use_fancy_tab_bar = false
+
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
 if wezterm.gui then
@@ -11,6 +13,17 @@ if wezterm.gui then
 		config.color_scheme = 'Tokyo Night Storm'
 	else
 		config.color_scheme = 'Tokyo Night Day'
+		
+		local bg_color = '#E1E2ED'
+		config.colors = {
+			tab_bar = {
+				background = bg_color,
+				new_tab = {
+					bg_color = '#007197',
+					fg_color = '#E9E9ED',
+				},
+			},
+		}
   	end
 else
 	config.color_scheme = 'Tokyo Night Storm'
