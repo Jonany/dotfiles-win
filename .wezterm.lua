09fd20226b1054dec3e5f199cf845c3ce47cd7b9
Jonathan Rigsby <Jonathan.Rigsby@softprocorp.com>
2025-02-13 13:22:16 -0500

    Added a hotkey to rename the current tab

--- .wezterm.lua
+++ .wezterm.lua
@@ -41,6 +41,21 @@ config.keys = {
   { key = 'DownArrow', mods = 'SHIFT', action = act.ScrollByLine(1) },
   { key = 'PageUp',    mods = 'SHIFT', action = act.ScrollByPage(-0.75) },
   { key = 'PageDown',  mods = 'SHIFT', action = act.ScrollByPage(0.75) },
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
 }
 
 -- and finally, return the configuration to wezterm
