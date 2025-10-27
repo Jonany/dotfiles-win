27e48cc85972ef2a99af1c0584c8b345a3d004a3
Jonathan Rigsby <Jonathan.Rigsby@softprocorp.com>
2025-10-27 11:26:47 -0400

    pwsh:ergonomic wrapper around dotnet build/test

---
+++ pwsh/better-test.ps1
@@ -0,0 +1,8 @@
+﻿Set-StrictMode -Version Latest
+
+function better-test() {
+    echo "Building..." `
+    && dotnet build --nologo --no-restore --tl:off -clp:"ErrorsOnly;NoSummary" -p:WarningLevel=0 `
+    && echo "Testing..." `
+    && dotnet test --no-build --nologo --tl:off --logger "console;verbosity=detailed" @args
+}
