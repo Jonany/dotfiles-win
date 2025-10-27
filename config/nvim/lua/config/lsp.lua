0f496093ff54963887090edbd9cb7dc62252d1c8
Jonathan Rigsby <Jonathan.Rigsby@softprocorp.com>
2025-10-27 14:40:25 -0400

    nvim:switched to using seblyng/roslyn for C# LSP

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -10,14 +10,61 @@
     }
   },
 }
-vim.lsp.config["csharp_ls"] = {
-  cmd = { 'C:/Users/Jonathan.Rigsby/.dotnet/tools/csharp-ls.exe' },
+-- plugin settings in plugins/rosyln.lua
+vim.lsp.config["roslyn"] = {
+  cmd = {
+    "dotnet",
+    "C:/development/usr-bin/Microsoft.CodeAnalysis.LanguageServer/content/LanguageServer/win-x64/Microsoft.CodeAnalysis.LanguageServer.dll",
+    "--logLevel",              -- this property is required by the server
+    "Information",
+    "--extensionLogDirectory", -- this property is required by the server
+    vim.fs.joinpath(vim.uv.os_tmpdir(), "roslyn_ls/logs"),
+    "--stdio",
+  },
   filetypes = { 'cs', 'csproj' },
   root_markers = { { '.csproj', '.sln' }, '.git' },
+  on_attach = function()
+    print("This will run when the server attaches!")
+  end,
+  settings = {
+    ["csharp|inlay_hints"] = {
+      csharp_enable_inlay_hints_for_implicit_object_creation = true,
+      csharp_enable_inlay_hints_for_implicit_variable_types = true,
+      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+      csharp_enable_inlay_hints_for_types = true,
+      dotnet_enable_inlay_hints_for_indexer_parameters = true,
+      dotnet_enable_inlay_hints_for_literal_parameters = true,
+      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+      dotnet_enable_inlay_hints_for_other_parameters = true,
+      dotnet_enable_inlay_hints_for_parameters = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+    },
+    ["csharp|code_lens"] = {
+      dotnet_enable_references_code_lens = true,
+      dotnet_enable_tests_code_lens = true,
+    },
+    ["csharp|background_analysis"] = {
+      dotnet_analyzer_diagnostics_scope = "openFiles",
+      dotnet_compiler_diagnostics_scope = "fullSolution",
+    },
+    ["csharp|completion"] = {
+      dotnet_provide_regex_completions = true,
+      dotnet_show_completion_items_from_unimported_namespaces = true,
+      dotnet_show_name_completion_suggestions = true,
+    },
+    ["csharp|symbol_search"] = {
+      dotnet_search_reference_assemblies = true,
+    },
+    ["csharp|formatting"] = {
+      dotnet_organize_imports_on_format = true,
+    },
+  }
 }
 
 vim.lsp.enable("lua-language-server")
-vim.lsp.enable("csharp_ls")
+vim.lsp.enable("roslyn")
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
