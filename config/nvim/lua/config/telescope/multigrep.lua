d738b4d9906487bd8c25ae5c3240a02cb1a0c5e0
Jonathan Rigsby <Jonathan.Rigsby@softprocorp.com>
2026-02-23 11:06:30 -0500

    nvim:multigrep remove local M var

--- config/nvim/lua/config/telescope/multigrep.lua
+++ config/nvim/lua/config/telescope/multigrep.lua
@@ -3,10 +3,9 @@ local finders = require('telescope.finders')
 local make_entry = require('telescope.make_entry')
 local conf = require('telescope.config').values
 
-local M = {}
-
 local live_multigrep = function(opts)
   opts = opts or {}
+  opts.args = opts.args or {}
   opts.cwd = opts.cwd or vim.uv.cwd()
 
   local finder = finders.new_async_job {
@@ -15,28 +14,30 @@ local live_multigrep = function(opts)
         return nil
       end
       local pieces = vim.split(prompt, '  ')
-      local args = { 'rg' }
+      local promptArgs = { 'rg' }
       if pieces[1] then
-        table.insert(args, '-e')
-        table.insert(args, pieces[1])
+        table.insert(promptArgs, '-e')
+        table.insert(promptArgs, pieces[1])
       end
 
       if #pieces > 1 then
         _ = table.remove(pieces, 1)
         for _, arg_piece in ipairs(pieces) do
-          table.insert(args, '-g')
-          table.insert(args, arg_piece)
+          table.insert(promptArgs, '-g')
+          table.insert(promptArgs, arg_piece)
         end
       end
 
       return vim.tbl_flatten {
-        args,
+        opts.args,
+        promptArgs,
         { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '--glob-case-insensitive' },
       }
     end,
     entry_maker = make_entry.gen_from_vimgrep(opts),
     cwd = opts.cwd,
   }
+
   pickers.new(opts, {
     debounce = 100,
     prompt_title = 'Multi Grep',
@@ -46,8 +47,10 @@ local live_multigrep = function(opts)
   }):find()
 end
 
-M.setup = function()
-  vim.keymap.set('n', '<leader>fg', live_multigrep)
-end
-
-return M
+return {
+  setup = function()
+    vim.keymap.set('n', '<leader>fg', live_multigrep)
+    -- Doesn't return any results
+    --vim.keymap.set('n', '<leader>fs', function() live_multigrep({ args = { '--sorted=path', }, }) end)
+  end
+}
