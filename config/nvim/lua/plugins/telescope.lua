64a4fa98378cf1d98a66264ff8127659dbf80279
Jonathan Rigsby <Jonathan.Rigsby@softprocorp.com>
2026-03-30 14:56:38 -0400

    nvim: customizing telescope windows

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,4 @@
-﻿return {
+return {
   'nvim-telescope/telescope.nvim',
   tag = 'v0.2.1',
   dependencies = {
@@ -10,10 +10,50 @@
     },
   },
   config = function()
+    -- Customizations
+    local actions = require('telescope.actions')
+    local action_layout = require("telescope.actions.layout")
+    require("telescope").setup {
+      defaults = {
+        --path_display = { shorten = { len = 1, exclude = { -1, -2 } } },
+        path_display = { "filename_first" },
+        layout_strategy = "vertical",
+        layout_config = {
+          vertical = {
+            preview_height = function(_, _, max_lines) return math.max(math.floor(max_lines * 0.66), 15) end,
+          },
+        },
+        mappings = {
+          n = {
+            -- M == meta/alt
+            ["<M-p>"] = action_layout.toggle_preview,
+          },
+          i = {
+            ["<M-p>"] = action_layout.toggle_preview,
+          },
+        },
+      },
+      pickers = {
+        buffers = {
+          mappings = {
+            i = {
+              ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+            },
+          },
+        },
+      },
+    }
+
+    -- Keymaps
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     --vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fb',
+      function()
+        builtin.buffers({ show_all_buffers = false, path_display = { "filename_first" }, })
+      end,
+      { desc = 'Telescope buffers' }
+    )
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     --vim.keymap.set('n', '<leader>en', function() builtin.find_files({ cwd = vim.fn.stdpath('config') }) end,
     --  { desc = 'Telescope Neovim config files' })
