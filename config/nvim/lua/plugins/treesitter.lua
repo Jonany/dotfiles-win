f268e3e946072ab7ba744e1f9732fcba18a6b5a0
Jonathan Rigsby <Jonathan.Rigsby@softprocorp.com>
2026-05-05 15:17:57 -0400

    nvim: update nvim-treesitter for v0.12

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,51 @@
-﻿return {
+return {
   {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
     config = function()
-      require 'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = {
-          "c",
-          "c_sharp",
-          "csv",
-          "diff",
-          "dockerfile",
-          "editorconfig",
-          "gitignore",
-          "go",
-          "gomod",
-          "gosum",
-          "html",
-          "javascript",
-          "jq",
-          "jsdoc",
-          "json",
-          "jsonc",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "powershell",
-          "psv",
-          "query",
-          "sql",
-          "toml",
-          "tsv",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "xml",
-          "yaml",
-        },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-          additional_vim_regex_highlighting = false,
-        },
+      require('nvim-treesitter').install {
+        'c',
+        'c_sharp',
+        'csv',
+        'diff',
+        'dockerfile',
+        'editorconfig',
+        'gitignore',
+        'go',
+        'gomod',
+        'gosum',
+        'html',
+        'javascript',
+        'jq',
+        'jsdoc',
+        'json',
+        'jsonc',
+        'lua',
+        'markdown',
+        'markdown_inline',
+        'powershell',
+        'psv',
+        'query',
+        'sql',
+        'toml',
+        'tsv',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
       }
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*',
+        callback = function(args)
+          local max_filesize = 1000000 -- 1 MB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+          if ok and stats and stats.size > max_filesize then return end
+          pcall(vim.treesitter.start)
+        end,
+      })
     end,
-  }
+  },
 }
